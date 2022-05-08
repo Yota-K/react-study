b46136171d46be4fb121c288ed79a5ea4e442add
Y.K <powdersugar828828@gmail.com>
2022-05-08 19:58:00 +0900

    feat: emotion入れてみて。

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Nav from "./Nav";
+import { css } from "@emotion/react";
 
 const Header: React.FC = () => {
+  const heading = css`
+    a {
+      color: #fff;
+    }
+  `;
+
   return (
-    <header>
-      <h1>
-        <Link to="/">React勉強会第2回</Link>
+    <header
+      css={css({
+        background: "#2196f3",
+        color: "#fff",
+        padding: "8px",
+      })}
+    >
+      <h1 css={heading}>
+        <Link to="/">React勉強会第4回</Link>
       </h1>
-      <div className="header-nav">
-        <Nav />
-      </div>
     </header>
   );
 };
